d93f161213d7f638ca922f4f4c4548a13b538350
Esteban Portillo <portilloesteban1@gmail.com>
2021-04-01 13:33:50 -0400

    app4  and app5 is done

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
+import Img from './Component/Img'
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [image, setImage] = useState('https://i.pinimg.com/originals/5f/bb/67/5fbb6710efa22f8633d6347a5e52c601.jpg')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Img image = {image} />
     </div>
   );
 }
---
+++ app-5/src/Component/Img.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Img(props) {
+    return (
+        <div>
+            <img src = {props.image } />
+        </div>
+    );
+}
+
+export default Img;
